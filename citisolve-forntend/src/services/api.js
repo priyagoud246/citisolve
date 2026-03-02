1f501db773d23e8b74a552de30ca276c75c31ed3
Sama <priyagoud246@gmail.com>
2026-03-02 13:53:40 +0530

    Update API URL for production

--- citisolve-forntend/src/services/api.js
+++ citisolve-forntend/src/services/api.js
@@ -1,6 +1,5 @@
 
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
 const getToken = () => localStorage.getItem("token");
 
 const callAPI = async (endpoint, options = {}) => {
